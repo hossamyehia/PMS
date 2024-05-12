6dc65e63dc153fa599e4e8bd42845aa626a52b8b
hossamyehia <hossamyehia74@yahoo.com>
2024-05-12 19:11:47 +0300

    Changed Application Arch

--- src/app/Modules/auth/auth.component.ts
+++ src/app/Modules/auth/auth.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from './services/auth.service';
+
 
 @Component({
   selector: 'app-auth',
@@ -9,28 +8,5 @@ import { AuthService } from './services/auth.service';
 })
 export class AuthComponent {
 
-  constructor(private _AuthService:AuthService){}
-  hide:boolean = true;
-
-  loginForm:FormGroup = new FormGroup({
-    email:new FormControl('', [Validators.required, Validators.email]),
-    password:new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,16}$/)])
-  })
-
-  sendLoginForm():void{
-    const data = this.loginForm.value;
-
-    if(this.loginForm.valid){
-      this._AuthService.login(data).subscribe({
-        next:(res)=>{
-          console.log(res);
-        },
-        error:(err)=>{
-          console.log(err);
-
-        }
-      })
-    }
-
-  }
+  constructor(){}
 }
