ff4c805e78e7addf8872221885b03f89324ee8bd
hossamyehia <hossamyehia74@yahoo.com>
2024-05-12 19:43:42 +0300

    Merged Verify Branch to main

--- src/app/Modules/auth/components/verify/verify.component.ts
+++ src/app/Modules/auth/components/verify/verify.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -7,34 +7,42 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './verify.component.html',
   styleUrls: ['./verify.component.scss']
 })
-export class VerifyComponent {
-  constructor(private _AuthService:AuthService){}
-  hide:boolean = true;
+export class VerifyComponent implements OnInit {
 
-  verifyForm:FormGroup = new FormGroup({
-    email:new FormControl('', [Validators.required, Validators.email]),
-    code:new FormControl('', [Validators.required])
+  bgImagePath = "url('assets/images/bg3.png')";
+  hide: boolean = true;
+  verifyForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    code: new FormControl('', [Validators.required])
   })
 
-  
-    sendVerifyForm():void{
-      const data = this.verifyForm.value;
-  
-      if(this.verifyForm.valid){
-        console.log(data)
-        this._AuthService.verify(data).subscribe({
-          next:(res)=>{
-            console.log(res);
-           // console.log('verified')
-          },
-          error:(err)=>{
-            console.log(err);
-  
-          }
-        })
-      }
-  
-    }
+  constructor(private _AuthService: AuthService) { }
+
+
+  ngOnInit(): void {
+    (document.querySelector(".auth-bg") as any).style.setProperty("--imagePath", `${this.bgImagePath}`)
   }
+
+
+
+  sendVerifyForm(): void {
+    const data = this.verifyForm.value;
+
+    if (this.verifyForm.valid) {
+      console.log(data)
+      this._AuthService.verify(data).subscribe({
+        next: (res) => {
+          console.log(res);
+          // console.log('verified')
+        },
+        error: (err) => {
+          console.log(err);
+
+        }
+      })
+    }
+
+  }
+}
 
 
