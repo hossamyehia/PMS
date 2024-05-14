34a344b6e1fd21d90a8e71967ad98e299cc6e97e
hossamyehia <hossamyehia74@yahoo.com>
2024-05-14 12:32:02 +0300

    Sidebar Apearence change

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { TokenService } from 'src/app/core';
 
-interface IMenu{
-  text:string;
-  icon:string;
-  link:string;
-  isActive:boolean
+interface IMenu {
+  text: string;
+  icon: string;
+  link: string;
+  isActive: boolean
 }
 
 @Component({
@@ -14,39 +14,39 @@ interface IMenu{
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  constructor(private _TokenService:TokenService){}
 
   menu: IMenu[] = [
-
     {
-      text:'Users',
-      icon:'fa-solid fa-user-group',
-      link:'/dashboard/manager/users',
-      isActive:this._TokenService.isManager()
+      text: 'Users',
+      icon: 'fa-thin fa-user-group',
+      link: '/dashboard/manager/users',
+      isActive: this._TokenService.isManager()
     },
     {
-      text:'Projects Manager',
-      icon:'fa-solid fa-diagram-project',
-      link:'/dashboard/manager/projects',
-      isActive:this._TokenService.isManager()
+      text: 'Projects',
+      icon: 'fa-thin fa-objects-column',
+      link: '/dashboard/manager/projects',
+      isActive: this._TokenService.isManager()
     },
     {
-      text:'Tasks Manager',
-      icon:'fa-solid fa-list-check',
-      link:'/dashboard/manager/tasks',
-      isActive:this._TokenService.isManager()
+      text: 'Tasks',
+      icon: 'fa-thin fa-list-check',
+      link: '/dashboard/manager/tasks',
+      isActive: this._TokenService.isManager()
     },
     {
-      text:'Projects Emp',
-      icon:'fa-solid fa-diagram-project',
-      link:'/dashboard/employee/projects',
-      isActive:this._TokenService.isEmployee()
+      text: 'Projects',
+      icon: 'fa-thin fa-objects-column',
+      link: '/dashboard/employee/projects',
+      isActive: this._TokenService.isEmployee()
     },
     {
-      text:'Taskes Emp',
-      icon:'fa-solid fa-list-check',
-      link:'/dashboard/employee/tasks',
-      isActive:this._TokenService.isEmployee()
+      text: 'Tasks',
+      icon: 'fa-thin fa-list-check',
+      link: '/dashboard/employee/tasks',
+      isActive: this._TokenService.isEmployee()
     },
   ]
+
+  constructor(private _TokenService: TokenService) { }
 }
