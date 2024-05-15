47eac3ffb13e8ab1fcb30ea2421f202ccd66f2da
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-05-16 00:20:03 +0300

    finishing table

---
+++ src/app/Modules/dashboard/subModules/manager/taskes/components/display-task/display-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-task',
+  templateUrl: './display-task.component.html',
+  styleUrls: ['./display-task.component.scss']
+})
+export class DisplayTaskComponent {
+
+}
--- src/app/Modules/dashboard/subModules/manager/taskes/taskes.module.ts
+++ src/app/Modules/dashboard/subModules/manager/taskes/taskes.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { TaskesRoutingModule } from './taskes-routing.module';
 import { TaskesComponent } from './taskes.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DisplayTaskComponent } from './components/display-task/display-task.component';
 
 
 @NgModule({
   declarations: [
-    TaskesComponent
+    TaskesComponent,
+    DisplayTaskComponent
   ],
   imports: [
     CommonModule,
