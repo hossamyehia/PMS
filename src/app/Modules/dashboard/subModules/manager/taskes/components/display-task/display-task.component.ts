39b862c8d649aeca62dfbb75716614d4dd8191fd
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-05-16 01:21:23 +0300

    adding display task component

--- src/app/Modules/dashboard/subModules/manager/taskes/components/display-task/display-task.component.ts
+++ src/app/Modules/dashboard/subModules/manager/taskes/components/display-task/display-task.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { iProject, iProjectResponse, iSearchableProject } from 'src/app/Modules/dashboard/shared/projects/models';
+import { ProjectService } from 'src/app/Modules/dashboard/shared/projects/services/project.service';
+import { TasksService } from 'src/app/Modules/dashboard/shared/tasks/services/tasks.service';
+import { HelperService } from 'src/app/core';
 
 @Component({
   selector: 'app-display-task',
@@ -7,4 +13,57 @@ import { Component } from '@angular/core';
 })
 export class DisplayTaskComponent {
 
+  view: string = 'false';
+  id!: number;
+  pageSize =1000 ;
+  pageNumber =1 ;
+  //for getting all projects
+  params: iSearchableProject = {
+    title: "",
+    pageSize: this.pageSize,
+    pageNumber: this.pageNumber,
+  };
+  Projectsdata:iProject[] =[];
+  taskForm: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    employeeId :new FormControl('', [Validators.required]),
+    projectId :new FormControl('', [Validators.required]),
+  })
+
+ 
+
+
+  constructor(
+    private _ProjectService: ProjectService,private _TasksService:TasksService,
+    private _route: ActivatedRoute,
+    private _helperService: HelperService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllProject();
+  }
+
+  onSubmit() {
+    
+  }
+
+  onAdd(data: iProject): void {
+    
+  }
+
+  onEdit(data: iProject): void {
+    
+  }
+
+  getAllProject() {
+    
+     this._ProjectService.getAllProjects(this.params).subscribe({
+      next: (res: iProjectResponse) => {
+        console.log(res)
+        this.Projectsdata = res.data;
+      }
+     });
+  } 
+    
 }
