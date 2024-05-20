365fee6801797b3789f4e01fa1ddf8bd79c3f69c
hossamyehia <hossamyehia74@yahoo.com>
2024-05-20 15:34:39 +0300

    Employee Project Module Done (#18)

--- src/app/Modules/dashboard/subModules/employee/projects/projects.component.ts
+++ src/app/Modules/dashboard/subModules/employee/projects/projects.component.ts
@@ -1,4 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { HelperService, iErrorResponse, iPage } from 'src/app/core';
+import Table from 'src/app/shared/table/model/table.model';
+import { iProjectResponse, iSearchableProject } from '../../../shared/projects/models';
+import { ProjectService } from '../../../shared/projects/services/project.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +14,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+
+
+  table: Table = {
+    colDefs: [
+      {
+        field: "title",
+        headerName: "Title",
+      },
+      {
+        field: "description",
+        headerName: "Description",
+      },
+      {
+        headerName: "Num Tasks",
+        valueGetter: (row: any) => {
+          return row.task.length;
+        }
+      },
+      {
+        field: "creationDate",
+        headerName: "Created At",
+        valueFormatter: (value: string): string => {
+          let pipe = new DatePipe("en")
+          return pipe.transform(value, 'fullDate') as string;
+        },
+      },
+      {
+        field: "modificationDate",
+        headerName: "Modified At",
+        valueFormatter: (value: string): string => {
+          let pipe = new DatePipe("en")
+          return pipe.transform(value, 'fullDate') as string;
+        }
+      },
+    ],
+    data: [],
+    operators: []
+  };
+
+  pagination: iPage = {
+    pageSize: 10,
+    pageNumber: 1,
+    totalNumberOfRecords: 0,
+    totalNumberOfPages: 0,
+  }
+
+  params: iSearchableProject = {
+    title: "",
+    pageSize: this.pagination.pageSize,
+    pageNumber: this.pagination.pageNumber,
+  };
+
+  constructor(
+    private _projectService: ProjectService,
+    private _helperSerivce: HelperService,
+    private _Router: Router,
+    public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects() {
+    this._projectService.getProjectsByEmployee(this.params).subscribe({
+      next: (res: iProjectResponse) => {
+        //console.log(res)
+        this.table.data = res.data;
+        this.pagination = (({ pageSize,
+          pageNumber,
+          totalNumberOfRecords,
+          totalNumberOfPages, ...rest }) => {
+          return {
+            pageSize,
+            pageNumber,
+            totalNumberOfRecords,
+            totalNumberOfPages,
+          }
+        })(res)
+      }, error: (err: iErrorResponse) => {
+        this._helperSerivce.openSnackBar(this._helperSerivce.getErrorMessage(err));
+      }
+    })
+  }
+
+  resetSearchInput() {
+    this.params.title = '';
+    this.getProjects();
+  }
+
+  changePage(e: PageEvent) {
+    this.params.pageNumber = e.pageIndex + 1;
+    this.params.pageSize = e.pageSize;
+    this.getProjects();
+  }
 
 }
